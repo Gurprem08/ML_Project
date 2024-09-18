adb1d0f535afcd777cb57171819712e1fd9cb51b
guru008 <gurpremsingh9@gmail.com>
2024-09-18 18:34:25 +0100

    Added hyperparameter tuning

--- components/utils.py
+++ components/utils.py
@@ -23,13 +23,17 @@ def save_object(file_path, obj):
         raise CustomException(e, sys)
     
 
-def evaluate_models(X_train,y_train,X_test,y_test,models):
+def evaluate_models(X_train,y_train,X_test,y_test,models,param):
     try:
         report ={
         }
 
         for i in range(len(list(models))):
             model = list(models.values())[i]
+            para=param[list(models.keys())[i]]
+            grid_search = GridSearchCV(model, para, cv=5, scoring='r2')
+            grid_search.fit(X_train,y_train)
+            model.set_params(**grid_search.best_params_) 
             model.fit(X_train,y_train)
 
             y_train_pred = model.predict(X_train)
